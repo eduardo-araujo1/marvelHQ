3c3d5cdd8c5c782efc71fe0383d5eb54375aace7
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-02-14 19:11:06 -0300

    Adicionando JWT na aplicação

--- src/main/java/com/eduardo/MarvelApi/controller/UserController.java
+++ src/main/java/com/eduardo/MarvelApi/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.eduardo.MarvelApi.controller;
 
 import com.eduardo.MarvelApi.dto.AuthenticationDTO;
+import com.eduardo.MarvelApi.dto.LoginResponseDTO;
 import com.eduardo.MarvelApi.dto.RegisterDTO;
+import com.eduardo.MarvelApi.infra.TokenService;
 import com.eduardo.MarvelApi.model.User;
 import com.eduardo.MarvelApi.repositories.UserRepository;
 import jakarta.validation.Valid;
@@ -22,13 +24,16 @@ public class UserController {
 
     private final AuthenticationManager authenticationManager;
     private final UserRepository repository;
+    private final TokenService tokenService;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO authenticationDTO){
         var usernamePassword = new UsernamePasswordAuthenticationToken(authenticationDTO.email(), authenticationDTO.password());
         var auth =  this.authenticationManager.authenticate(usernamePassword);
 
-        return ResponseEntity.ok().build();
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @PostMapping("/register")
---
+++ src/main/java/com/eduardo/MarvelApi/dto/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.eduardo.MarvelApi.dto;
+
+public record LoginResponseDTO(String token) {
+}
